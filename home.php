c32d1b2a9856dd583146a1388ded0e75de61150e
Even <kiki44552002@gmail.com>
2015-04-03 13:57:52 +0800

    hotfix for losing </div> tag

--- home.php
+++ home.php
@@ -13,16 +13,18 @@
   $myImg="";
 
   while($res = $result->fetch_array(MYSQLI_BOTH)){
-    $myImg .= '<div class="row">
-    <div class="col-md-12">
-      <div class="thumbnail">
-        <img src="' .$S3_URL.'user_upload/'.$res['s3_link'].'" alt="...">
-        <div class="caption">
-          <h3>' .$res['title']. '</h3>
-          <p>' .$res['caption']. '</p>
+    $myImg .= '
+      <div class="row">
+        <div class="col-md-12">
+          <div class="thumbnail">
+            <img src="' .$S3_URL.'user_upload/'.$res['s3_link'].'" alt="...">
+          </div>
+          <div class="caption">
+            <h3>' .$res['title']. '</h3>
+            <p>' .$res['caption']. '</p>
+          </div>
         </div>
-      </div>
-    </div>';
+      </div>';
   }
   $result->close();
 
@@ -88,7 +90,7 @@
       </form>
     </div>
 
-    <div class="col-md-8">
+    <div class="col-md-7 col-md-offset-1">
       <?php
         echo $myImg;
       ?>
